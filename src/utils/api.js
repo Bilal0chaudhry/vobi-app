2e7fd64cc80f344671f46ac0b45211b14a2b8c52
Bilal Chaudhry <chaudhrybilal1977@gmail.com>
2026-08-07 02:02:00 +0500

    Update frontend for Cloudflare tunnel

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8000';
+const API_BASE = 'https://protein-pack-her-mention.trycloudflare.com';
 
 export async function queryAvailityEligibility(job) {
   const res = await fetch(`${API_BASE}/availity/eligibility`, {
